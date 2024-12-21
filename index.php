9ac10ca82a7c73c498430d8c7038009d9dc54faf
MiguelA83 <gabrielyael6@gmail.com>
2024-12-20 21:50:42 -0600

    formulario de login

--- index.php
+++ index.php
@@ -4,10 +4,32 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+<ul class="list-group">
+  <li class="list-group-item"><form>
+  <fieldset disabled>
+   
+    <div class="mb-3">
+      
+      <input type="text" id="disabledTextInput" class="form-control" placeholder="Correo electronico o número de telefono">
+    </div>
+    <div class="mb-3">
+      
+            <input type="text" id="disabledTextInput" class="form-control" placeholder="Contraseña">
 
-
-
-
+        
+      
+    </div>
+    <center>
+    <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+  </fieldset></center>
+  <center><h6>¿Olvidaste tu contraseña?</h6></center>
+</form></li>
+<center>
+ <a class="nav-link active" aria-current="page" href="crearcuenta.php">Crear cuenta nueva</a>
+  <center><h6>Crear cuenta nueva
+Crea una página para una celebridad, una marca o un negocio</h6></center>
+  </center>
+</ul>
 
 
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
